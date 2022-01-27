cd01a821c39d7d960941ea94e286e364b2074273
EC2 Default User <ec2-user@ip-172-31-84-89.ec2.internal>
2022-01-27 08:14:26 +0000

    途中save

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -6,7 +6,7 @@ class Admin::GenresController < ApplicationController
   end
   
   def create
-    genre = Genre.new(genre.params)
+    genre = Genre.new(genre_params)
     genre.save
     redirect_to admin_genres_path
     
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_25_123441) do
+ActiveRecord::Schema.define(version: 2022_01_27_051346) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -46,6 +46,17 @@ ActiveRecord::Schema.define(version: 2022_01_25_123441) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.integer "genre_id"
+    t.string "name"
+    t.string "image_id"
+    t.text "introduction"
+    t.integer "price"
+    t.boolean "is_active", default: true
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
