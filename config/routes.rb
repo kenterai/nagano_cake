c65ffb60b1fd384994be044a7d35a20606bb2899
EC2 Default User <ec2-user@ip-172-31-84-89.ec2.internal>
2022-02-02 12:49:44 +0000

    [Add]マイページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
   root to: "homes#top"
   get '/about' => 'homes#about',as:'about'
-  
+
+  namespace :admin do
+    resources :genres, only:[:index, :create, :edit, :update]
+    resources :items, only:[:index, :new, :create, :show, :edit, :update]
+    resources :customers, only:[:index, :show, :edit, :update]
+  end
+
+  scope module: :public do
+    resource :customers, only:[:edit, :update, :quit, :out]
+    get 'customers/my_page' => 'customers#show'
+  end
+
   devise_for :customers, controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
@@ -11,11 +22,6 @@ Rails.application.routes.draw do
     sessions: "admin/sessions"
   }
 
-  namespace :admin do
-    resources :genres, only:[:index, :create, :edit, :update]
-    resources :items, only:[:index, :new, :create, :show, :edit, :update]
-    resources :customers, only:[:index, :show, :edit, :update]
-  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
